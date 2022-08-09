ca994a5f28e8bc2c63040afc71fcfda5223c7f7e
Stanislav Dmitruk <stasdmitruk1@gmail.com>
2022-08-09 03:49:44 +0200

    Added draft for the setup method in TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,20 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import drivers.LocalMobileDriver;
+import org.junit.jupiter.api.BeforeAll;
+
 public class TestBase {
+
+    @BeforeAll
+    public static void setup() {
+        Configuration.browserSize = null;
+
+        switch (System.getProperty("device")) {
+            case "emulator":
+                Configuration.browser = LocalMobileDriver.class.getName();
+                break;
+
+        }
+    }
 }
